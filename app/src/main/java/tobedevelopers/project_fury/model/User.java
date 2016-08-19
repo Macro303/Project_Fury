e45fd884cc7f4aedcd93a94308c011839ec22fe3
Macro303 <jackson.jonah@gmail.com>
2016-08-19 13:38:27 +1200

    FURY#012 - Create a Web Service #50 Create Model Initial Update for DB

--- app/src/main/java/tobedevelopers/project_fury/model/User.java
+++ app/src/main/java/tobedevelopers/project_fury/model/User.java
@@ -2,11 +2,21 @@ package tobedevelopers.project_fury.model;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.Date;
+
 /**
  * Created by Macro303 on 16/08/2016.
  */
 public class User{
 
+	private String username;
+	private String password;
+	private Boolean admin;
+	private String email;
+	@SerializedName( "created_at" )
+	private Date createdAt;
+	@SerializedName( "updated_at" )
+	private Date updatedAt;
 	private int userID;
 	@SerializedName( "first-name" )
 	private String firstName;
@@ -17,6 +27,15 @@ public class User{
 		this.userID = userID;
 		this.firstName = firstName;
 		this.lastName = lastName;
+	}
+
+	public User( Boolean admin, Date createdAt, String email, Date updatedAt, String password, String username ){
+		this.admin = admin;
+		this.createdAt = createdAt;
+		this.email = email;
+		this.updatedAt = updatedAt;
+		this.password = password;
+		this.username = username;
 	}
 
 	public int getUserID(){
@@ -31,10 +50,44 @@ public class User{
 		return lastName;
 	}
 
-	@Override
+	public Boolean getAdmin(){
+		return admin;
+	}
+
+	public Date getCreatedAt(){
+		return createdAt;
+	}
+
+	public String getEmail(){
+		return email;
+	}
+
+	public String getPassword(){
+		return password;
+	}
+
+	public Date getUpdatedAt(){
+		return updatedAt;
+	}
+
+	public String getUsername(){
+		return username;
+	}
+
+	/*@Override
 	public String toString(){
 		return "User ID: " + userID +
 			       "\nFirst Name: " + firstName +
 			       "\nLast Name: " + lastName;
+	}*/
+
+	@Override
+	public String toString(){
+		return "Username: " + username +
+			       "\nPassword: " + password +
+			       "\nEmail: " + email +
+			       "\nAdmin: " + admin +
+			       "\nCreated At: " + createdAt +
+			       "\nUpdated At: " + updatedAt;
 	}
 }
