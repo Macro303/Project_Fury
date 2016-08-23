cc20adce7dd6aed74a552258bb95a92e0b4ca205
Andrea <awolff@live.ca>
2016-08-23 17:19:36 +1200

    FURY#004 - Create a project #65 - Create XML Created xml layout of activity. #67 - Field and Button validation Created entry fields and validated the entered data. #66 - Create activity code Created view, presenter, contract code for the create project info screen

--- app/src/main/java/tobedevelopers/project_fury/create_project/implementation/CreateProjectPresenter.java
+++ app/src/main/java/tobedevelopers/project_fury/create_project/implementation/CreateProjectPresenter.java
@@ -3,6 +3,8 @@ package tobedevelopers.project_fury.create_project.implementation;
 import java.lang.ref.WeakReference;
 
 import tobedevelopers.project_fury.create_project.CreateProjectContract;
+import tobedevelopers.project_fury.model.Model;
+import tobedevelopers.project_fury.model.ModelContract;
 
 /**
  * Created by Macro303 on 11/08/2016.
@@ -11,10 +13,12 @@ public class CreateProjectPresenter implements CreateProjectContract.Presenter{
 
 	private WeakReference< CreateProjectContract.View > viewWeakReference;
 	private WeakReference< CreateProjectContract.Navigation > navigationWeakReference;
+	private ModelContract modelContract;
 
 	public CreateProjectPresenter( CreateProjectContract.View view, CreateProjectContract.Navigation navigation ){
 		this.viewWeakReference = new WeakReference<>( view );
 		this.navigationWeakReference = new WeakReference<>( navigation );
+		this.modelContract = new Model();
 	}
 
 	@Override
@@ -23,7 +27,70 @@ public class CreateProjectPresenter implements CreateProjectContract.Presenter{
 		CreateProjectContract.Navigation navigation = navigationWeakReference.get();
 
 		if( view != null && navigation != null ){
+//			new AsyncTask< String, Void, Response >(){
+//
+//				@Override
+//				protected void onPreExecute(){
+//					CreateProjectContract.View view = viewWeakReference.get();
+//
+//					if( view != null ){
+//						view.projectCreationInProgress();
+//					}
+//				}
+//
+//				@Override
+//				protected Response doInBackground( String... strings ){
+//					return modelContract.
+//				}
+//
+//				@Override
+//				protected void onPostExecute( Response response ){
+//					RegisterContract.View view = viewWeakReference.get();
+//					RegisterContract.Navigation navigation = navigationWeakReference.get();
+//
+//					if( view != null ){
+//						if( response.getError().equals( "Passed" ) ){
+//							navigation.navigateToLogin();
+//						}else if( response.getError().equals( "No Internet Access" ) ){
+//							view.noInternetAccessValidation();
+//						}else{
+//							view.setUsernameAlreadyUsedValidation();
+//						}
+//					}
+//				}
+//			}.executeOnExecutor( AsyncTask.THREAD_POOL_EXECUTOR );
 			navigation.navigateToPrevious();
+		}
+	}
+
+	@Override
+	public void userEnterProjectName( String projectName ){
+		CreateProjectContract.View view = viewWeakReference.get();
+
+		if( view != null ){
+			if( projectName.length() >= 3 && projectName.length() < 20 ){
+				view.enableCreateProjectButton();
+			}else if( projectName.length() < 3 ){
+				view.setProjectNameUnder3CharValidation();
+				view.disableCreateProjectButton();
+			}else if( projectName.length() > 19 ){
+				view.setProjectNameOver20CharValidation();
+				view.disableCreateProjectButton();
+			}
+		}
+	}
+
+	@Override
+	public void userEnterProjectDescription( String projectDescription ){
+		CreateProjectContract.View view = viewWeakReference.get();
+
+		if( view != null ){
+			if( projectDescription.length() < 128 ){
+				view.enableCreateProjectButton();
+			}else if( projectDescription.length() > 127 ){
+				view.setProjectDescriptionOver128CharValidation();
+				view.disableCreateProjectButton();
+			}
 		}
 	}
 }
--- app/src/main/java/tobedevelopers/project_fury/create_project/implementation/CreateProjectView.java
+++ app/src/main/java/tobedevelopers/project_fury/create_project/implementation/CreateProjectView.java
@@ -1,12 +1,17 @@
 package tobedevelopers.project_fury.create_project.implementation;
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.text.Editable;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import butterknife.OnTextChanged;
 import tobedevelopers.project_fury.BaseView;
 import tobedevelopers.project_fury.R;
 import tobedevelopers.project_fury.Runnable1Param;
@@ -16,6 +21,13 @@ import tobedevelopers.project_fury.create_project.CreateProjectContract;
  * Created by Macro303 on 11/08/2016.
  */
 public class CreateProjectView extends BaseView implements CreateProjectContract.View, CreateProjectContract.Navigation{
+
+	@Bind( R.id.createProjectActivity_projectNameEditText )
+	TextInputEditText mProjectNameEditText;
+	@Bind( R.id.createProjectActivity_projectDescriptionEditText )
+	TextInputEditText mProjectDescriptionEditText;
+	@Bind( R.id.createProjectActivity_createProjectButton )
+	Button mCreateProjectButton;
 
 	private CreateProjectContract.Presenter presenter;
 
@@ -45,6 +57,17 @@ public class CreateProjectView extends BaseView implements CreateProjectContract
 		}
 	}
 
+	//Text Change Listener
+	@OnTextChanged( value = { R.id.createProjectActivity_projectNameEditText }, callback = OnTextChanged.Callback.AFTER_TEXT_CHANGED )
+	public void onUserChangedProjectNameEditText( Editable editable ){
+		presenter.userEnterProjectName( editable.toString() );
+	}
+
+	@OnTextChanged( value = { R.id.createProjectActivity_projectDescriptionEditText }, callback = OnTextChanged.Callback.AFTER_TEXT_CHANGED )
+	public void onUserChangedProjectDescriptionEditText( Editable editable ){
+		presenter.userEnterProjectDescription( editable.toString() );
+	}
+
 	@Override
 	public void navigateToPrevious(){
 		runOnUiThread( new Runnable1Param< CreateProjectView >( this ){
@@ -53,5 +76,70 @@ public class CreateProjectView extends BaseView implements CreateProjectContract
 				finish();
 			}
 		} );
+	}
+
+	@Override
+	public void enableCreateProjectButton(){
+		runOnUiThread( new Runnable1Param< CreateProjectView >( this ){
+			@Override
+			public void run(){
+				getParam1().mCreateProjectButton.setEnabled( true );
+			}
+		} );
+	}
+
+	@Override
+	public void disableCreateProjectButton(){
+		runOnUiThread( new Runnable1Param< CreateProjectView >( this ){
+			@Override
+			public void run(){
+				getParam1().mCreateProjectButton.setEnabled( false );
+			}
+		} );
+	}
+
+	@Override
+	public void projectCreationInProgress(){
+		runOnUiThread( new Runnable1Param< CreateProjectView >( this ){
+			@Override
+			public void run(){
+				Toast.makeText( getParam1(), getString( R.string.error_projectCreationInProgress ), Toast.LENGTH_SHORT ).show();
+			}
+		} );
+	}
+
+	@Override
+	public void setProjectNameUnder3CharValidation(){
+		runOnUiThread( new Runnable1Param< TextInputEditText >( mProjectNameEditText ){
+			@Override
+			public void run(){
+				getParam1().setError( getString( R.string.error_min3Characters ) );
+			}
+		} );
+	}
+
+	@Override
+	public void setProjectNameOver20CharValidation(){
+		runOnUiThread( new Runnable1Param< TextInputEditText >( mProjectNameEditText ){
+			@Override
+			public void run(){
+				getParam1().setError( getString( R.string.error_max20Characters ) );
+			}
+		} );
+	}
+
+	@Override
+	public void setProjectDescriptionOver128CharValidation(){
+		runOnUiThread( new Runnable1Param< TextInputEditText >( mProjectDescriptionEditText ){
+			@Override
+			public void run(){
+				getParam1().setError( getString( R.string.error_max128Characters ) );
+			}
+		} );
+	}
+
+	@Override
+	public void noInternetAccessValidation(){
+		Toast.makeText( this, getString( R.string.error_noInternetAccess ), Toast.LENGTH_LONG ).show();
 	}
 }
